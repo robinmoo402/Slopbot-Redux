ab115c50db04c942aeb214327105bb3583f43d8f
Aaron Moore <aaronmoo@udel.edu>
2023-08-19 17:14:09 -0400

    test command

---
+++ README.md
@@ -0,0 +1,2 @@
+little personal project, not taking this too seriously 
+much of the initial code comes from the discord.js tutorial
---
+++ commands/test/testcommand.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('test')
+        .setDescription('basic test command'),
+    async execute(interaction) {
+        await interaction.reply('yuri has conquered the earth');
+    }
+}
